00d7a91b085d5a4e99025ec6c3fbb484f070e218
Bernardo Macedo <bernardo-macedo@hotmail.com>
2019-02-18 23:46:05 -0300

    Add app icons (default and round) Add app logo as placeholder to articles without image

--- app/src/main/java/com/bmacedo/hiremenews/utils/ViewExtensions.kt
+++ app/src/main/java/com/bmacedo/hiremenews/utils/ViewExtensions.kt
@@ -1,7 +1,9 @@
 package com.bmacedo.hiremenews.utils
 
 import android.widget.ImageView
+import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.BindingAdapter
+import com.bmacedo.hiremenews.R
 import com.bumptech.glide.Glide
 
 @BindingAdapter("bind:imageUrl")
@@ -9,6 +11,6 @@ fun loadImage(view: ImageView, url: String?) {
     if (url != null && url.isNotEmpty()) {
         Glide.with(view.context).load(url).into(view)
     } else {
-        view.setImageDrawable(null)
+        view.setImageDrawable(ResourcesCompat.getDrawable(view.context.resources, R.drawable.logo, null))
     }
 }
